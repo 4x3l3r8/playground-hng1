0dc940bc267e09b9674c63879cd1adf5bb3c46e6
Adesanya Daniel <4x3l3r8@gmail.com>
2022-10-30 23:17:31 +0100

    imported data list from requirements

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const LinkData = [
+    {
+        id: 1,
+        link: "https://twitter.com/NielAdax",
+        name: "Twitter Link",
+        element_id: "twitter-link",
+    },
+    {
+        id: 2,
+        link: "https://training.zuri.team",
+        name: "Zuri Team",
+        element_id: "btn__zuri",
+    },
+    {
+        id: 3,
+        link: "https://books.zuri.team",
+        name: "Zuri Books",
+        element_id: "books",
+    },
+    {
+        id: 4,
+        link: "https://books.zuri.team/python-for-beginners?ref_id=danny-el",
+        name: "Python Books",
+        element_id: "book__python",
+    },
+    {
+        id: 5,
+        link: "https://background.zuri.team",
+        name: "Background Check for Coders",
+        element_id: "pitch",
+    },
+    {
+        id: 6,
+        link: "https://books.zuri.team/design-rules",
+        name: "Design Books",
+        element_id: "book__design",
+    },
+];
